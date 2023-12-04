4ffafa56271f7898a9b07e111c7d28eb42166d33
aaronfeingold <ajfeingold88@gmail.com>
2023-12-04 16:47:10 -0500

    building out the carousel with some simple use state to toggle which image we are viewing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/24/solid';
+import presidentRanchi from './assets/president_ranchi.jpeg';
+import spookyRanchi from './assets/spooky_ranchi.jpeg';
+import ranchiAdmiringRanchi from './assets/ranchi_admiring_ranchi.jpeg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface CarouselProps {
+    slides: string[];
 }
 
-export default App
+function Carousel ({ slides }: CarouselProps) {
+    const [current, setCurrent] = useState(0);
+
+    const previousSlide = () => {
+        if (current === 0) setCurrent(slides.length - 1);
+        else setCurrent(current - 1)
+    }
+
+    const nextSlide = () => {
+        if (current === slides.length - 1) setCurrent(0);
+        else setCurrent(current + 1)
+    }
+
+    return (
+            <div className="overflow-hidden relative">
+                <div
+                    className={`flex transition east-out duration-400`}
+                    style ={{
+                        transform: `translateX(-${current * 100}]%)`
+                    }}
+                >
+                    {slides.map((s, i) => {
+                        return <img key={i} src={s} alt={`Slide ${i}`}/>;
+                    })}
+                </div>
+
+                <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl">
+                    <button onClick={previousSlide}>
+                        <ArrowLongLeftIcon />
+                    </button>
+                    <button onClick={nextSlide}>
+                        <ArrowLongRightIcon />
+                    </button>
+                </div>
+            </div>
+    )
+}
+
+function App() {
+    const slides: string[] = [
+        presidentRanchi,
+        spookyRanchi,
+        ranchiAdmiringRanchi,
+    ];
+
+    return (
+        <div className="w-[60%] m-auto pt-11">
+            <Carousel slides={slides} />
+        </div>
+    )
+
+}
+
+export default App;
